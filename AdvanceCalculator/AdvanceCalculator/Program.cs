e50e3c707fa39279c4aa6e3789e0a31cb92851df
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-01-27 09:29:17 +0100

    Update Program.cs

--- AdvanceCalculator/AdvanceCalculator/Program.cs
+++ AdvanceCalculator/AdvanceCalculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
 
 class Program
 {
@@ -6,72 +7,108 @@ class Program
     {
         string operation = Console.ReadLine();
         Interpret(operation);
+
     }
 
     static void Interpret(string operation)
     {
-        string[] lines = operation.Split('\n');
+        
+
+        List<string> lines = new List<string>(operation.Split('\n'));
+
+
 
         foreach (string line in lines)
         {
-            string[] tokens = line.Split(' ');
+
+            List<string> tokens = new List<string>(line.Split(' '));
+
+            double a;
+            double b;
+
+            if (tokens[0] == "pi")
+            {
+                tokens[0] = "3,14";
+            }            
+            if (tokens[1] == "pi")
+            {
+                tokens[1] = "3,14";
+            }
+            if (tokens[2] == "pi")
+            {
+                tokens[2] = "3,14";
+            }
+
+
 
             if (tokens[1] == "+")
             {
-                double result = Double.Parse(tokens[0]) + Double.Parse(tokens[2]);
+                a = double.Parse(tokens[0]);
+                b = double.Parse(tokens[2]);
+                double result = a + b;
                 Console.WriteLine(">> " + result);
             }
             else if (tokens[1] == "-")
             {
-                double result = Double.Parse(tokens[0]) - Double.Parse(tokens[2]);
+                a = double.Parse(tokens[0]);
+                b = double.Parse(tokens[2]);
+                double result = a - b;
                 Console.WriteLine(">> " + result);
             }
             else if (tokens[1] == "*")
             {
-                double result = Double.Parse(tokens[0]) * Double.Parse(tokens[2]);
+                a = double.Parse(tokens[0]);
+                b = double.Parse(tokens[2]);
+                double result = a * b;
                 Console.WriteLine(">> " + result);
             }
             else if (tokens[1] == "/")
             {
-                double result = Double.Parse(tokens[0]) / Double.Parse(tokens[2]);
+                a = double.Parse(tokens[0]);
+                b = double.Parse(tokens[2]);
+                double result = a / b;
                 Console.WriteLine(">> " + result);
             }
             else if (tokens[1] == "^")
             {
-                double n1 = Double.Parse(tokens[0]);
-                double n2 = Double.Parse(tokens[2]);
-                double result = (double)Math.Pow(n1, n2);
+                a = double.Parse(tokens[0]);
+                b = double.Parse(tokens[2]);
+                
+                double result = (double)Math.Pow(a, b);
 
                 Console.WriteLine(">> " + result);
             }
             else if (tokens[1] == "root")
             {
-                double n1 = Double.Parse(tokens[0]);
-                double n2 = Double.Parse(tokens[2]);
-                double result = Math.Pow(n1, 1.0 / n2);
+                a = double.Parse(tokens[0]);
+                b = double.Parse(tokens[2]);
+                double result = Math.Pow(a, 1.0 / b);
                 Console.WriteLine(">> " + result);
 
             }
             else if (tokens[0] == "|" && tokens[2] == "|")
             {
-                double n1 = Double.Parse(tokens[1]);
-                double result = (double)Math.Abs(n1);
+                a = double.Parse(tokens[1]);
+                double result = (double)Math.Abs(a);
                 Console.WriteLine(">> " + result);
-            }        
+            }
             else if (tokens[0] == "log_10")
             {
-                double n1 = Double.Parse(tokens[1]);
-                double result = (double)Math.Log10(n1);
+                a = double.Parse(tokens[1]);
+                double result = (double)Math.Log10(a);
                 Console.WriteLine(">> " + result);
             }
             else if (tokens[0] == "log_2")
             {
-                double n1 = Double.Parse(tokens[1]);
-                double result = (double)Math.Log2(n1);
+                a = double.Parse(tokens[1]);
+                double result = (double)Math.Log2(a);
                 Console.WriteLine(">> " + result);
             }
+
         }
     }
+
+    
 
 
 }
